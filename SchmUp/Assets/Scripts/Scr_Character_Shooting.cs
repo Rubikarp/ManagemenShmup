f7158ccc2c1b499134f026e2a64ede82e9738e27
Luc <l.vidal@rubika-edu.com>
2020-02-12 16:52:27 +0100

    Tir OK

--- SchmUp/Assets/Scripts/Scr_Character_Shooting.cs
+++ SchmUp/Assets/Scripts/Scr_Character_Shooting.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Scr_Character_Shooting : MonoBehaviour
 {
+    public GameObject projectile;
+    private bool goodToShoot;
+    public float bulletDelay;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        goodToShoot = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) && goodToShoot == true)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            goodToShoot = false;
+            StartCoroutine(BulletDelay());
+        }
+    }
+
+    IEnumerator BulletDelay()
+    {
+        yield return new WaitForSeconds(bulletDelay);
+        goodToShoot = true;
     }
 }
---
+++ SchmUp/Assets/Scripts/Scr_Projectile_Behaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scr_Projectile_Behaviour : MonoBehaviour
+{
+    public float speed;
+    public float damage;
+    private Rigidbody2D bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bullet.velocity = new Vector2(0, speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Scr_Enemy_Death>().health -= damage;
+            Destroy(gameObject);
+        }
+    }
+}
